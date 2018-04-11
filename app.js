4c50027b4f27ff6b8eeb38784ce8c238ee13b76d
mikael <mikaelgueguen1@gmail.com>
2018-04-11 11:01:10 +0200

    collision

--- app.js
+++ app.js
@@ -1,62 +1,61 @@
 $("#start").click(function () {
     $('#sound')[0].play();
 
+
     var manger = 1;
 
-    function start()
+    function fall()
 
-    console.log('start')
+    console.log('fall')
 
     {
 
-//la methode animate déplace. Cette methode permet de déplacer l'objet. Ici c'est un donut que l'on déplace vers le bas.
+        //la methode animate déplace. Cette methode permet de déplacer l'objet. Ici c'est un donut que l'on déplace vers le bas.
 
-    $('#donut').animate({ bottom: '100px' }, 2000, 'linear', function () {
+        $('#donut').animate({ bottom: '100px' }, 2000, 'linear', function () {
 
-console.log('donut')
+            console.log('donut')
 
-//le donut se déplace aléatoirement de gauche à droite
+            //le donut se déplace aléatoirement de gauche à droite
 
-        var donutX = Math.floor(Math.random() * 400);
+            var donutX = Math.floor(Math.random() * 400);
 
-        var donutY = 1000;
+            var donutY = 1000;
 
-        $('#donut').css('bottom', donutY);
+            $('#donut').css('bottom', donutY);
 
-        $('#donut').css('left', donutX);
+            $('#donut').css('left', donutX);
 
-        manger = 1;
+            manger = 1;
 
-        
-    });
-    start();
 
-    console.log('start')
+        });
+        fall();
+
+        console.log('fall')
     };
-    
+
 });
 
 
 
-$(document).keydown(function(e){
+$(document).keydown(function (e) {
 
 
-//pour connaitre à quoi corresponde les touches il faut aller sur
+    //pour connaitre à quoi corresponde les touches il faut aller sur
     //https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_event_key_which2
 
 
 
     //ici c'est pour la flèche de droite
 
-    if (e.which == 39)
+    if (e.which == 39) {
 
-    {
+        homerX = parseInt($('#homer').css('left'));
 
-      homerX = parseInt($('#homer').css('left'));
+        if (homerX < 490)
 
-      if (homerX < 490)
-
-      $('#homer').css('left', homerX+30);
+            $('#homer').css('left', homerX + 30);
 
     }
 
@@ -66,28 +65,43 @@ $(document).keydown(function(e){
 
     {
 
-      homerX = parseInt($('#homer').css('left'));
+        homerX = parseInt($('#homer').css('left'));
 
-      if (homerX > 10)
+        if (homerX > 10)
 
-        $('#homer').css('left', homerX-30);
+            $('#homer').css('left', homerX - 30);
+
+    }
+
+    if (e.which == 13) {
+        $("#start").click();
 
     }
 
 });
 
 
-
-
-
-
-
-//<input type=text id=champTexte onkeydown=if (event.keyCode == 13) document.getElementById('bouton').click()/>
-//<input type=button id=bouton value=Valider onclick=maFonction(); />
-//Vous pouvez également utiliser la bibliothèque jQuery afin de raccourcir le code grâce au sélecteur :
-//$(#champTexte).keydown(function(event){
-//if(event.keyCode == 13){
-//$(#bouton).click();
+//if (object1.x < object2.x + object2.width && object1.x + object1.width > object2.x &&
+  //  object1.y < object2.y + object2.height && object1.y + object1.height > object2.y) {
+    // The objects are touching
 //}
-//});
+
+
+function eat()
+{
+    homerX = parseInt($('#homer').css('left'));
+    donutX = parseInt($('#donut').css('left'));
+    homerY = 10;
+    donutY = parseInt($('#donut').css('bottom'));
+
+    if (homer.x<donut.x + donut.width66 && homer.x + homer.width66 > donut.x &&
+    homer.y < donut.y + donut.height15 && homer.y + homer.height15 > donut.y){
+
+        $('#info').text(manger);
+
+        consol.log('text')
+
+        manger = 0;
+    }
+}
 
